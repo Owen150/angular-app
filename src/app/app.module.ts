45b2634fa6b3468b987827cfbe26dfca5a543e32
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-03-01 14:56:54 +0300

    Initiated Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
 import { FormsModule } from '@angular/forms';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
-import { TestComponentComponent } from './test-component/test-component.component';
-import { FormsModule } from '@angular/forms';
-import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
+import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AttributeDirectivesComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
     TestComponentComponent,
-    StructuralDirectivesComponent
+    StructuralDirectivesComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
