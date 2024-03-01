cff094dd6c35f1372d66ff7ae00be7c24b659e91
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-03-01 14:56:54 +0300

    Merged All Branches into Main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
 import { FormsModule } from '@angular/forms';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
-import { TestComponentComponent } from './test-component/test-component.component';
-import { FormsModule } from '@angular/forms';
-import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
+import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AttributeDirectivesComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
     TestComponentComponent,
     StructuralDirectivesComponent
   ],
