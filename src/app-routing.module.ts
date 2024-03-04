a5ec086fb389028618583f56b3efc5b4764fbcf8
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-03-04 14:44:39 +0300

    Completed Routing

---
+++ src/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './app/home/home.component';
+import { AboutComponent } from './app/about/about.component';
+import { ContactComponent } from './app/contact/contact.component';
+import { ProductComponent } from './app/product/product.component';
+import { ProductsComponent } from './app/products/products.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: 'product/:id', component: ProductComponent},
+  { path: '**', redirectTo: 'home'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
