2cffdb934151f8a57f846cd7302851946f551d6d
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-03-04 17:29:06 +0300

    App Updates - Reactive Forms Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TestComponentComponent } from './test-component/test-component.component';
 import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
 import { ParentComponent } from './parent/parent.component';
@@ -31,7 +31,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
-export class ReactiveComponent {
+export class ReactiveComponent implements OnInit {
+  myForm!: FormGroup;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      uname: new FormControl('Michael Owen', Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      readTerms: new FormControl(false),
+    });
+  }
+
+  onSubmit() {
+    this.myForm.reset();
+  }
+
+  accept() {
+    this.myForm.setValue({
+      readTerms: true,
+    });
+  }
 }
