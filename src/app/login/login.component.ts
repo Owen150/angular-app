bc181d9d26f33fed96a294af06508448f73fa6aa
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-03-07 10:29:42 +0300

    Auth Service Updates

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +12,27 @@ export class LoginComponent implements OnInit {
   password = '';
   errorMsg = '';
 
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   login() {
+    // Login Validation
     if (this.username.trim().length === 0) {
-      this.errorMsg = "Username is Required";
+      this.errorMsg = 'Username is Required';
     } else if (this.password.trim().length === 0) {
-      this.errorMsg = "Password is Required";
+      this.errorMsg = 'Password is Required';
     } else {
-      this.errorMsg = "";
+      this.errorMsg = '';
+      // Inject Auth Service if no validation errors occur
+      let res = this.authService.login(this.username, this.password);
+      if (res === 200) {
+        //Route the user to Homepage
+        this.router.navigate(['home']);
+      }
+      if (res === 403) {
+        this.errorMsg = 'Invalid Credentials';
+      }
     }
   }
 }
